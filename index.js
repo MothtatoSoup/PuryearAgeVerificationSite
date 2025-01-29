732e7d7d55032780eea4ef3ef9d83356e9f65a4a
MothtatoSoup <maddy.puryear@outlook.com>
2025-01-29 11:18:12 -0500

    spelling

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ userSubmit.onclick = function() {
     if (age > 200){
         document.getElementById("myResult").textContent = `Wow. You are the oldest person on earth. What wisdom do you hold?`;
     } else if (age >= 100){
-        document.getElementById("myResult").textContent = `Wow you're old. Potetially the eldest person on earth`;
+        document.getElementById("myResult").textContent = `Wow you're old. Potentially the eldest person on earth`;
     } else if (age <= 99){
         document.getElementById("myResult").textContent = "How is life so far?";
     }  else if (age == 0){
