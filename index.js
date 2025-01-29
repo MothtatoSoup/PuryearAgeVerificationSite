81009d09ab0f6ef4270e6cd94c351130c05d3db7
MothtatoSoup <maddy.puryear@outlook.com>
2025-01-29 11:21:46 -0500

    Changed order of if statements

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ userSubmit.onclick = function() {
         document.getElementById("myResult").textContent = `Wow. You are the oldest person on earth. What wisdom do you hold?`;
     } else if (age >= 100){
         document.getElementById("myResult").textContent = `Wow you're old. Potentially the eldest person on earth`;
-    } else if (age <= 99){
-        document.getElementById("myResult").textContent = "How is life so far?";
     }  else if (age == 0){
         document.getElementById("myResult").textContent = `You're a baby. How are you using this?`;
     }  else if (age < 0){
         document.getElementById("myResult").textContent = `Erm you aren't even born yet`;
+    } else if (age <= 99){
+        document.getElementById("myResult").textContent = "How is life so far?";
     }
 }
