e5018aa27679b780a3c68418041cd609b3c66ce8
MothtatoSoup <maddy.puryear@outlook.com>
2025-01-29 11:12:41 -0500

    Works :D

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 "use strict";
 
-document.getElementById("myButton").addEventListener("click", function (age) {
-    let age = document.getElementById("userAge").value;
+const userAgeInput = document.getElementById("myInput");
+const userSubmit = document.getElementById("myButton");
+const userResult = document.getElementById("myResult");
+
+let age;
+
+userSubmit.onclick = function() {
+    age = userAgeInput.value;
     age = Number(age);
     if (age > 200){
-        document.getElementById("userResult").textContent = `Wow. You are the oldest person on earth. What wisdom do you hold?`;
+        document.getElementById("myResult").textContent = `Wow. You are the oldest person on earth. What wisdom do you hold?`;
     } else if (age >= 100){
-        document.getElementById("userResult").textContent = `wowie you're olddd. Potetially the eldest person on earth`;
+        document.getElementById("myResult").textContent = `wowie you're olddd. Potetially the eldest person on earth`;
     } else if (age <= 99){
-        document.getElementById("userResult").textContent = `How is life so far?`
+        document.getElementById("myResult").textContent = "How is life so far?";
     }  else if (age == 0){
-        document.getElementById("userResult").textContent = `You're a baby. How are you using this?`;
+        document.getElementById("myResult").textContent = `You're a baby. How are you using this?`;
     }  else if (age < 0){
-        document.getElementById("userResult").textContent = `Erm you aren't even born yet`;
+        document.getElementById("myResult").textContent = `Erm you aren't even born yet`;
     }
-});
+}
